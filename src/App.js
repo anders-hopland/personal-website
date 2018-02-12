e63fb5d74fec5a714bf1da0a566480d55f169cf6
anders-hopland <andershopl@hotmail.com>
2018-02-12 23:41:48 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import FrontPage from "./Components/frontpage.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FrontPage />
+        <Switch>
+        
+          <Route exact path='/' component={ FrontPage }/>
+    
+        </ Switch>
       </div>
     );
   }
---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class FrontPage extends Component {
+
+  render () {
+    return (
+      <div className="front-splash">
+        <h1> DEVELOPER </ h1>
+      </ div>
+    )
+  }
+}
+
+export default FrontPage 
